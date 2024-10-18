a2738a7c46291d03dc2160cf4e8c3a6647be7955
salvoparisi <psalvo065@gmail.com>
2024-10-18 14:15:14 +0200

    js code optimization

--- assets/main.js
+++ assets/main.js
@@ -6,24 +6,18 @@ fetch('https://jsonplaceholder.typicode.com/photos?_limit=6')
 .then(response => response.json())
 .then(data=>{
     data.forEach(obj =>{
-        titlePosts.push(obj.title)
-    });
-    data.forEach(obj =>{
-        imgPosts.push(obj.thumbnailUrl)
-    })
-    for (let i = 0; i < data.length; i++) {
         main.innerHTML+=`
             <div class="cards">
                 <img src="./assets/img/pin.svg" class="pin" alt="">
                 <div class="m-3 p-3 card-text bg-white shadow-lg">
                     <div class="card-img bg-primary">
-                        <img src="${imgPosts[i]}" class="img-in">
+                        <img src="${obj.thumbnailUrl}" class="img-in">
                     </div>
-                    <div class="pt-2">${titlePosts[i]}</div>
+                    <div class="pt-2">${obj.title}</div>
                 </div>
             </div>
-        `;
-    }
+        `
+    })
 });
 
 
